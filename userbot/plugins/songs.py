cefcc1cb5a87b29e8c92ee4430cdeafeff4f8bc6
jasem-iq <83676607+jasem-iq@users.noreply.github.com>
2021-05-12 22:11:26 +0300

    Update songs.py

--- userbot/plugins/songs.py
+++ userbot/plugins/songs.py
@@ -1,7 +1,4 @@
-"""
-By  : @RRRD7 ( https://t.me/Jmthon )
-Edit: @SBB_B ( https://t.me/SBB_B  )
-"""
+# Jmthon
 
 import asyncio
 import base64
@@ -12,26 +9,22 @@ from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon.tl.functions.messages import ImportChatInviteRequest as Get
 from validators.url import url
 
-from . import (
-    mention, 
-    name_dl, 
-    song_dl, 
-    video_dl, 
-    yt_search, 
-)
+from . import name_dl, song_dl, video_dl, yt_search
+
+# =========================================================== #
+#                           STRINGS                           #
+# =========================================================== #
+SONG_SEARCH_STRING = "<code>wi8..! I am finding your song....</code>"
+SONG_NOT_FOUND = "<code>Sorry !I am unable to find any song like that</code>"
+SONG_SENDING_STRING = "<code>yeah..! i found something wi8..🥰...</code>"
+SONGBOT_BLOCKED_STRING = "<code>Please unblock @songdl_bot and try again</code>"
+# =========================================================== #
+#                                                             #
+# =========================================================== #
 
 
-@icssbot.on(
-    icss_cmd(
-       pattern="(بحث|اغنيه)($| (.*))"
-    )
-)
-@icssbot.on(
-    sudo_cmd(
-       pattern="(بحث|اغنيه)($| (.*))", 
-       allow_sudo=True, 
-    )
-)
+@bot.on(admin_cmd(pattern="(اغنيه|song320)($| (.*))"))
+@bot.on(sudo_cmd(pattern="(اغنيه|song320)($| (.*))", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
@@ -43,61 +36,61 @@ async def _(event):
         if reply.message:
             query = reply.message
     else:
-        await eor(event, "** ⌔∮ يجب عليك كتابت اسم الاغنيه لكي اقوم بالبحث عنها 🖤،** `")
+        await edit_or_reply(event, "`What I am Supposed to find `")
         return
-    ics = base64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
-    icse = await eor(event, "**  جاري البحث عن الاغنـيه ⌁**")
+    cat = base64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
+    catevent = await edit_or_reply(event, "**⌁ جـاري البـحث عـن ألاغنـية **")
     video_link = await yt_search(str(query))
     if not url(video_link):
-        return await icse.edit(
-            f"**آسف** {mention}\n **-لا يمكنني العثور على أي #فيديو او #صوت كهذا ⌁ \n - `{query}` "
+        return await catevent.edit(
+            f"Sorry!. I can't find any related video/audio for `{query}`"
         )
     cmd = event.pattern_match.group(1)
-    if cmd == "بحث":
+    if cmd == "song":
         q = "128k"
-    elif cmd == "اغنيه":
+    elif cmd == "song320":
         q = "320k"
     song_cmd = song_dl.format(QUALITY=q, video_link=video_link)
     # thumb_cmd = thumb_dl.format(video_link=video_link)
     name_cmd = name_dl.format(video_link=video_link)
     try:
-        ics = Get(ics)
-        await event.client(ics)
+        cat = Get(cat)
+        await event.client(cat)
     except BaseException:
         pass
-    stderr = (await _icssutils.runcmd(song_cmd))[1]
+    stderr = (await _catutils.runcmd(song_cmd))[1]
     if stderr:
-        return await icse.edit(f"** ⌁ خطا :** `{stderr}`")
-    icsn, stderr = (await _icssutils.runcmd(name_cmd))[:2]
+        return await catevent.edit(f"**Error :** `{stderr}`")
+    catname, stderr = (await _catutils.runcmd(name_cmd))[:2]
     if stderr:
-        return await icse.edit(f"** ⌁ خطا :** `{stderr}`")
+        return await catevent.edit(f"**Error :** `{stderr}`")
     # stderr = (await runcmd(thumb_cmd))[1]
-    icsn = os.path.splitext(icsn)[0]
+    catname = os.path.splitext(catname)[0]
     # if stderr:
-    #    return await icse.edit(f"** ⌁ خطا :** `{stderr}`")
-    song_file = Path(f"{icsn}.mp3")
+    #    return await catevent.edit(f"**Error :** `{stderr}`")
+    song_file = Path(f"{catname}.mp3")
     if not os.path.exists(song_file):
-        return await icse.edit(
-            f"**⌁ آسف** {mention}\n **-لا يمكنني العثور على أي #فيديو او #صوت كهذا \n - `{query}` ⇲"
+        return await catevent.edit(
+            f"Sorry!. I can't find any related video/audio for `{query}`"
         )
-    await icse.edit("** ⌁ جاري تحميل الاغنيه انتظر قليلا ⌁ .**")
-    icsthb = Path(f"{icsn}.jpg")
-    if not os.path.exists(icsthb):
-        icsthb = Path(f"{icsn}.webp")
-    elif not os.path.exists(icsthb):
-        icsthb = None
+    await catevent.edit("** ⌁ جاري تحميل الأغنيـة انتـظر قليلآ  **")
+    catthumb = Path(f"{catname}.jpg")
+    if not os.path.exists(catthumb):
+        catthumb = Path(f"{catname}.webp")
+    elif not os.path.exists(catthumb):
+        catthumb = None
 
     await event.client.send_file(
         event.chat_id,
         song_file,
         force_document=False,
-        caption=f"**⌁ الاغنيه :** {query}\n**⌁ للمستخدم :** {mention}",
-        thumb=icsthb,
+        caption=query,
+        thumb=catthumb,
         supports_streaming=True,
         reply_to=reply_to_id,
     )
-    await icse.delete()
-    for files in (icsthb, song_file):
+    await catevent.delete()
+    for files in (catthumb, song_file):
         if files and os.path.exists(files):
             os.remove(files)
 
@@ -111,20 +104,12 @@ async def delete_messages(event, chat, from_message):
     await event.client.send_read_acknowledge(chat)
 
 
-@icssbot.on(
-    icss_cmd(
-       pattern="يوتيوب( (.*)|$)"
-    )
-)
-@icssbot.on(
-    sudo_cmd(
-       pattern="يوتيوب( (.*)|$)", allow_sudo=True
-    )
-)
+@bot.on(admin_cmd(pattern="vsong( (.*)|$)"))
+@bot.on(sudo_cmd(pattern="vsong( (.*)|$)", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
-    rd = await reply_id(event)
+    reply_to_id = await reply_id(event)
     reply = await event.get_reply_message()
     if event.pattern_match.group(1):
         query = event.pattern_match.group(1)
@@ -132,116 +117,152 @@ async def _(event):
         if reply.message:
             query = reply.messag
     else:
-        event = await eor(event, "What I am Supposed to find")
+        event = await edit_or_reply(event, "What I am Supposed to find")
         return
-    ics = base64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
-    icse = await eor(event, "** ⌁ جاري البحث عن الفيديو ⌁ ،**")
+    cat = base64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
+    catevent = await edit_or_reply(event, "** جـاري البـحث عـن ألاغنـية **")
     video_link = await yt_search(str(query))
     if not url(video_link):
-        return await icse.edit(
-            f"**⌁ آسف** {mention}\n **-لا يمكنني العثور على أي #فيديو او #صوت كهذا \n - `{query}` ⌁"
+        return await catevent.edit(
+            f"Sorry!. I can't find any related video/audio for `{query}`"
         )
     # thumb_cmd = thumb_dl.format(video_link=video_link)
     name_cmd = name_dl.format(video_link=video_link)
     video_cmd = video_dl.format(video_link=video_link)
-    stderr = (await _icssutils.runcmd(video_cmd))[1]
+    stderr = (await _catutils.runcmd(video_cmd))[1]
     if stderr:
-        return await icse.edit(f"** ⌁ خطا :** `{stderr}`")
-    icsn, stderr = (await _icssutils.runcmd(name_cmd))[:2]
+        return await catevent.edit(f"**Error :** `{stderr}`")
+    catname, stderr = (await _catutils.runcmd(name_cmd))[:2]
     if stderr:
-        return await icse.edit(f"** ⌁ خطا :** `{stderr}`")
+        return await catevent.edit(f"**Error :** `{stderr}`")
     # stderr = (await runcmd(thumb_cmd))[1]
     try:
-        ics = Get(ics)
-        await event.client(ics)
+        cat = Get(cat)
+        await event.client(cat)
     except BaseException:
         pass
     # if stderr:
-    #    return await icse.edit(f"** ⌁ خطا :** `{stderr}`")
-    icsn = os.path.splitext(icsn)[0]
-    vsong_file = Path(f"{icsn}.mp4")
+    #    return await catevent.edit(f"**Error :** `{stderr}`")
+    catname = os.path.splitext(catname)[0]
+    vsong_file = Path(f"{catname}.mp4")
     if not os.path.exists(vsong_file):
-        vsong_file = Path(f"{icsn}.mkv")
+        vsong_file = Path(f"{catname}.mkv")
     elif not os.path.exists(vsong_file):
-        return await icse.edit(
-            f"**⌁ آسف** {mention}\n **-لا يمكنني العثور على أي #فيديو او #صوت كهذا \n - `{query}` ⇲"
+        return await catevent.edit(
+            f"Sorry!. I can't find any related video/audio for `{query}`"
         )
-    await icse.edit("** ⌁ جاري تحميل الفيديو انتظر قليلا ⌁ .**")
-    icsthb = Path(f"{icsn}.jpg")
-    if not os.path.exists(icsthb):
-        icsthb = Path(f"{icsn}.webp")
-    elif not os.path.exists(icsthb):
-        icsthb = None
+    await catevent.edit("**جاري تحميل الأغنيـة انتـظر قليلآ  📲**")
+    catthumb = Path(f"{catname}.jpg")
+    if not os.path.exists(catthumb):
+        catthumb = Path(f"{catname}.webp")
+    elif not os.path.exists(catthumb):
+        catthumb = None
     await event.client.send_file(
         event.chat_id,
         vsong_file,
         force_document=False,
-        caption=f"**⌁ البحث :** {query}\n**⌁ للمستخدم :** {mention}",
-        thumb=icsthb,
+        caption=query,
+        thumb=catthumb,
         supports_streaming=True,
-        reply_to=rd,
+        reply_to=reply_to_id,
     )
-    await icse.delete()
-    for files in (icsthb, vsong_file):
+    await catevent.delete()
+    for files in (catthumb, vsong_file):
         if files and os.path.exists(files):
             os.remove(files)
 
 
-@icssbot.on(admin_cmd(pattern="اغنيه (.*)"))
-@icssbot.on(sudo_cmd(pattern="اغنيه (.*)", allow_sudo=True))
-async def icssongfetcer(event):
+@bot.on(admin_cmd(pattern="song2 (.*)"))
+@bot.on(sudo_cmd(pattern="song2 (.*)", allow_sudo=True))
+async def cat_song_fetcer(event):
     if event.fwd_from:
         return
     song = event.pattern_match.group(1)
     chat = "@songdl_bot"
     reply_id_ = await reply_id(event)
-    icse = await eor(event, SONG_SEARCH_STRING, parse_mode="html")
+    catevent = await edit_or_reply(event, SONG_SEARCH_STRING, parse_mode="html")
     async with event.client.conversation(chat) as conv:
         try:
             purgeflag = await conv.send_message("/start")
             await conv.get_response()
             await conv.send_message(song)
-            tosh = await conv.get_response()
-            while tosh.edit_hide != True:
+            hmm = await conv.get_response()
+            while hmm.edit_hide != True:
                 await asyncio.sleep(0.1)
-                tosh = await event.client.get_messages(chat, ids=hmm.id)
+                hmm = await event.client.get_messages(chat, ids=hmm.id)
             baka = await event.client.get_messages(chat)
             if baka[0].message.startswith(
-                (f"**⌁ اسف** {mention}\n **- لم اعـثر على اي شي ⇱.**")
+                ("I don't like to say this but I failed to find any such song.")
             ):
                 await delete_messages(event, chat, purgeflag)
-                return await ed(
-                    icse, SONG_NOT_FOUND, parse_mode="html", time=5
+                return await edit_delete(
+                    catevent, SONG_NOT_FOUND, parse_mode="html", time=5
                 )
-            await icse.edit(SONG_SENDING_STRING, parse_mode="html")
+            await catevent.edit(SONG_SENDING_STRING, parse_mode="html")
             await baka[0].click(0)
+            await conv.get_response()
+            await conv.get_response()
             music = await conv.get_response()
             await event.client.send_read_acknowledge(conv.chat_id)
         except YouBlockedUserError:
-            await icse.edit(SONGBOT_BLOCKED_STRING, parse_mode="html")
+            await catevent.edit(SONGBOT_BLOCKED_STRING, parse_mode="html")
             return
         await event.client.send_file(
             event.chat_id,
             music,
-            caption=f"**⌁ الاغنيه :** {query}\n**⌁ للمستخدم :** {mention}",
+            caption=f"<b>➥ Song :- <code>{song}</code></b>",
             parse_mode="html",
             reply_to=reply_id_,
         )
-        await icse.delete()
+        await catevent.delete()
         await delete_messages(event, chat, purgeflag)
+
+
+@bot.on(admin_cmd(pattern="szm$", outgoing=True))
+@bot.on(sudo_cmd(pattern="szm$", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    if not event.reply_to_msg_id:
+        await edit_delete(event, "```Reply to an audio message.```")
+        return
+    reply_message = await event.get_reply_message()
+    chat = "@auddbot"
+    catevent = await edit_or_reply(event, "```Identifying the song```")
+    async with event.client.conversation(chat) as conv:
+        try:
+            await conv.send_message("/start")
+            await conv.get_response()
+            await conv.send_message(reply_message)
+            check = await conv.get_response()
+            if not check.text.startswith("Audio received"):
+                return await catevent.edit(
+                    "An error while identifying the song. Try to use a 5-10s long audio message."
+                )
+            await catevent.edit("Wait just a sec...")
+            result = await conv.get_response()
+            await event.client.send_read_acknowledge(conv.chat_id)
+        except YouBlockedUserError:
+            await catevent.edit("```Please unblock (@auddbot) and try again```")
+            return
+    namem = f"**Song Name : **`{result.text.splitlines()[0]}`\
+        \n\n**Details : **__{result.text.splitlines()[2]}__"
+    await catevent.edit(namem)
 
 
 CMD_HELP.update(
     {
-        "songs": "**Plugin : **`songs`\
-        \n\n  •**Syntax : **`.song <query/reply>`\
-        \n  •**Function : **__searches the song you entered in query from youtube and sends it, quality of it is 128k__\
-        \n\n  •**Syntax : **`.song320 <query/reply>`\
-        \n  •**Function : **__searches the song you entered in query from youtube and sends it quality of it is 320k__\
-        \n\n  •**Syntax : **`.vsong <query/reply>`\
-        \n  •**Function : **__Searches the video song you entered in query and sends it__\
-        \n\n  •**Syntax : **`.song2 query`\
-        \n  •**Function : **__searches the song you entered in query and sends it quality of it is 320k__\
+        "تحميل اغنية": "**التطبيق : **`تحميل اغنية`\
+        \n\n•  **Syntax : **`.song <query/reply>`\
+        \n•  **Function : **__searches the song you entered in query from youtube and sends it, quality of it is 128k__\
+        \n\n•  **Syntax : **`.song320 <query/reply>`\
+        \n•  **Function : **__searches the song you entered in query from youtube and sends it quality of it is 320k__\
+        \n\n•  **Syntax : **`.vsong <query/reply>`\
+        \n•  **Function : **__Searches the video song you entered in query and sends it__\
+        \n\n•  **Syntax : **`.song2 query`\
+        \n•  **Function : **__searches the song you entered in query and sends it quality of it is 320k__\
+        \n\n**•  Syntax : **`.szm` reply to an audio file\
+        \n**•  Function :**Reverse searchs of song/music\
         "
     }
 )
