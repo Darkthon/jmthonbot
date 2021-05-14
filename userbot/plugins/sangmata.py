3f6cee054adfd35cb9c3f0166f1e245ff2cb4a84
jasem-iq <83676607+jasem-iq@users.noreply.github.com>
2021-05-14 22:39:36 +0300

    Update and rename sangmata_AR.py to sangmata.py

--- userbot/plugins/sangmata.py
+++ userbot/plugins/sangmata.py
@@ -5,8 +5,8 @@ from telethon.errors.rpcerrorlist import YouBlockedUserError
 from . import parse_pre, sanga_seperator
 
 
-@bot.on(admin_cmd(pattern="(sg|sgu)($| (.*))"))
-@bot.on(sudo_cmd(pattern="(sg|sgu)($| (.*))", allow_sudo=True))
+@bot.on(admin_cmd(pattern="(تاريخ|sgu)($| (.*))"))
+@bot.on(sudo_cmd(pattern="(تاريخ|sgu)($| (.*))", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
@@ -15,7 +15,7 @@ async def _(event):
     if not input_str and not reply_message:
         await edit_delete(
             event,
-            "`الرد على الرسالة النصية للمستخدم للحصول على سجل الاسم / اسم المستخدم أو إعطاء معرف المستخدم / اسم المستخدم`",
+            "** قم بالـرد على الشخص للحصول على معلوماته ⌁**",
         )
     if input_str:
         try:
@@ -25,13 +25,13 @@ async def _(event):
                 u = await event.client.get_entity(input_str)
             except ValueError:
                 await edit_delete(
-                    event, "`أعط معرف المستخدم أو اسم المستخدم للعثور على سجل الاسم`"
+                    event, "**اعطي ايدي او معرف الشخص لايجاد معلوماته"
                 )
             uid = u.id
     else:
         uid = reply_message.sender_id
     chat = "@SangMataInfo_bot"
-    catevent = await edit_or_reply(event, "`جـاري البـحث ⌁...`")
+    catevent = await edit_or_reply(event, "**⌁**")
     async with event.client.conversation(chat) as conv:
         try:
             await conv.send_message(f"/search_id {uid}")
@@ -46,12 +46,12 @@ async def _(event):
             responses.append(response.text)
         await event.client.send_read_acknowledge(conv.chat_id)
     if not responses:
-        await edit_delete(catevent, "`لا يستطيع الروبوت جلب النتائج`")
+        await edit_delete(catevent, "هنالك خطا غير معروف>")
     if "No records found" in responses:
-        await edit_delete(catevent, "`المستخدم ليس لديه أي سجل`")
+        await edit_delete(catevent, "** هذا المستخدم ليس لديه اي تاريخ قديم**")
     names, usernames = await sanga_seperator(responses)
     cmd = event.pattern_match.group(1)
-    if cmd == "sg":
+    if cmd == "تاريخ":
         sandy = None
         for i in names:
             if sandy:
