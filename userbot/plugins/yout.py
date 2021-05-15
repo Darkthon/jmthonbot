cfe277b503dcde544a4248b9eb153ff631c54b32
jasem-iq <83676607+jasem-iq@users.noreply.github.com>
2021-05-15 22:40:08 +0300

    Update yout.py

--- userbot/plugins/yout.py
+++ userbot/plugins/yout.py
@@ -24,8 +24,8 @@ from youtube_dl.utils import (
 from . import hmention, progress, ytsearch
 
 
-@bot.on(admin_cmd(pattern="تحميل(ص|ف)(?: |$)(.*)", outgoing=True))
-@bot.on(sudo_cmd(pattern="تحميل(ص|ف)(?: |$)(.*)", allow_sudo=True))
+@bot.on(admin_cmd(pattern="تشغيل(ص|ف)(?: |$)(.*)", outgoing=True))
+@bot.on(sudo_cmd(pattern="تشغيل(ص|ف)(?: |$)(.*)", allow_sudo=True))
 async def download_video(v_url):
     """ For .ytdl command, download media from YouTube and many other sites. """
     url = v_url.pattern_match.group(2)
