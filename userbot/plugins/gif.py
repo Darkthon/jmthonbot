368507d63f41768aae3a063e487e69eacf3a5338
jasem-iq <83676607+jasem-iq@users.noreply.github.com>
2021-05-24 19:49:24 +0300

    Add files via upload

--- userbot/plugins/gif.py
+++ userbot/plugins/gif.py
@@ -237,7 +237,12 @@ async def amireallyalive(alive):
             f"**  -  𝗦𝗼𝘂𝗿𝗰𝗲 𝗖𝗵𝗮𝗻𝗻𝗲𝗹 :  @Jmthon**",
         )
    
-
+@borg.on(admin_cmd("م25"))
+async def _(event):
+     if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
+        await event.edit("لـعࢪض اۅٛمـر المـتحركات  :\n\n `.متحركه 1`\n `.متحركه 2`\n `.متحركه 3`\n `.متحركه 4`\n `.متحركه 5`\n `.متحركه 6`\n `.متحركه 7`\n `.متحركه 8`\n `.متحركه 9`\n `.متحركه 10`
+        
+        
 def check_data_base_heal_th():
     # https://stackoverflow.com/a/41961968
     is_database_working = False
